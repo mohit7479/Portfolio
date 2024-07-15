6446bf0cf93d1e74aba0e160ac05159c3d311bdd
mohit7479 <mohit7479936440@gmail.com>
2024-07-15 17:47:44 +0530

    changes

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:hneelabh/Portfolio.git main:gh-pages
+# git push -f git@github.com:mohit7479/Portfolio.git main:gh-pages
 
 cd -
